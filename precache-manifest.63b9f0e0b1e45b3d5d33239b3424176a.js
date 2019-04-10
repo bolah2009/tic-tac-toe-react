b6cdb64b39cb83a115198568cd95dc50d1a4a632
Buari Bola Ahmed <bola_ahmed2002@yahoo.com>
2019-04-10 13:20:02 +0100

    Updates

---
+++ precache-manifest.63b9f0e0b1e45b3d5d33239b3424176a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c650f4249a1a9ac15f21",
+    "url": "/tic-tac-toe-react/static/js/runtime~main.27a0c5ca.js"
+  },
+  {
+    "revision": "690eaf20a1494e59f3af",
+    "url": "/tic-tac-toe-react/static/js/main.957e87bf.chunk.js"
+  },
+  {
+    "revision": "685d0c597e8419129599",
+    "url": "/tic-tac-toe-react/static/js/2.0e3f6a1a.chunk.js"
+  },
+  {
+    "revision": "690eaf20a1494e59f3af",
+    "url": "/tic-tac-toe-react/static/css/main.154cbe81.chunk.css"
+  },
+  {
+    "revision": "4eb7819d3f3107ae6793c3154bda09c7",
+    "url": "/tic-tac-toe-react/index.html"
+  }
+];

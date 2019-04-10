c453734839e870145300b7b7a1dc90912c568de8
Buari Bola Ahmed <bola_ahmed2002@yahoo.com>
2019-04-10 12:29:38 +0100

    Add feature for flipping X and O - my version

--- src/index.js
+++ src/index.js
@@ -1,24 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-class Square extends React.Component {
-  render() {
+function Square (props){
     return (
-      <button className="square">
-        {this.props.value}
+      <button className="square" onClick={props.onClick}>
+        {props.value}
       </button>
     );
   }
-}
+
 
 class Board extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      squares: Array(9).fill(null),
+      isXNext: true,
+    };
+  }
+
+  handleClick(i){
+    const SQUARES = this.state.squares.slice();
+    SQUARES[i] = this.state.isXNext ? "X" : "O";
+    const isXNext = !this.state.isXNext;
+    this.setState({squares: SQUARES, isXNext: isXNext});
+  }
+
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+    <Square value={this.state.squares[i]} 
+                    onClick={() => this.handleClick(i)} 
+                    />
+    );
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = "Next player: X";
 
     return (
       <div className="board">
@@ -61,7 +79,4 @@ class Game extends React.Component {
 
 // ========================================
 
-ReactDOM.render(
-  <Game />,
-  document.getElementById('root')
-);
+ReactDOM.render(<Game />, document.getElementById("root"));
